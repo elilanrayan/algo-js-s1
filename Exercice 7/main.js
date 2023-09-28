e7f2dc2972fc6feeac08cc23024a860138df49ba
elilanrayan <65083041+elilanrayan@users.noreply.github.com>
2023-09-28 20:51:46 +0200

    Delete Exercice 7 directory

--- Exercice 7/main.js
+++
@@ -1,7 +0,0 @@
-let mainCharacter = {
-    firstName : "Rayan",
-    lastName:"Arasamohan",
-    age: 21,
-}
-
-console.log("Le personnage principal est "+ mainCharacter.firstName +""+ mainCharacter.lastName+""+ "et il a "+ mainCharacter.age+" ans")
